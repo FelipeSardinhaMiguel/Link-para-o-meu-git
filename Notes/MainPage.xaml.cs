a1527778684efd6904a9a93078b0de623123f169
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2025-08-18 22:34:13 -0300

    salvamento

---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string caminho = Path.Combine(FileSystem.AppDataDirectory, "arquivo");
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ApagarTextBtn_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void SalvarTextBtn_Clicked(object sender, EventArgs e)
+        {
+            String conteudo = CaixaEditor.Text;
+            File.WriteAllText(caminho, conteudo);
+
+
+
+
+        }
+    }
+
+}
